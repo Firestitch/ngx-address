f7b17783b3545ad3d4685b9e8a8321d049d4a17c
Anatoly <mendor1990@gmail.com>
2018-05-17 14:11:00 +0300

    export issue

--- src/fs-address.module.ts
+++ src/fs-address.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { JsonpModule } from '@angular/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {} from '@types/googlemaps';
-export declare var google: any;
 
 import {
   AgmCoreModule,
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import {} from '@types/googlemaps';
+declare var google: any;
+
 export * from './fs-address.module';
 export * from './constants';
 export * from './interfaces';
