fb86744a4b363212d2a932aaf4cb6647072c3af2
Raymond Gigliotti <ray@firestitch.com>
2026-06-04 15:10:39 -0400

    refactor googlePlaceToFsAddress function to simplify address handling and improve readability.

--- src/app/helpers/google-place-to-address.ts
+++ src/app/helpers/google-place-to-address.ts
@@ -5,13 +5,12 @@ import { FsAddress } from '../interfaces/address.interface';
 
 export function googlePlaceToFsAddress(
   result: google.maps.places.Place,
-  config: FsAddressConfig,
+  _config: FsAddressConfig,
 ): FsAddress {
   const address = createEmptyAddress();
 
-  let countryLongName: string, regionLongName: string, streetShortName: string;
-
   address.id = result.id;
+  address.name = result.displayName;
   address.lat = result.location.lat();
   address.lng = result.location.lng();
   address.description = result.formattedAddress;
@@ -20,12 +19,10 @@ export function googlePlaceToFsAddress(
   result.addressComponents.forEach((item) => {
     if (item.types.some(type => type === 'country')) {
       address.country = item.shortText;
-      countryLongName = item.longText;
     }
 
     if (item.types.some(type => type === 'administrative_area_level_1')) {
       address.region = item.shortText;
-      regionLongName = item.longText;
     }
 
     if (item.types.some(type => type === 'locality' || type === 'political')) {
@@ -43,12 +40,10 @@ export function googlePlaceToFsAddress(
 
   if (streetNumber) {
     address.street = streetNumber.longText + ' ';
-    streetShortName = streetNumber.longText + ' ';
   } else {
     const match = address.description.match(/^[\d-]+/);
     if (match) {
       address.street = match[0] + ' ';
-      streetShortName = match[0] + ' ';
     }
   }
 
@@ -58,30 +53,9 @@ export function googlePlaceToFsAddress(
   if (streetAddress) {
     if (!address.street) {
       address.street = streetAddress.longText;
-      streetShortName = streetAddress.shortText;
     } else {
       address.street += streetAddress.longText;
-      streetShortName += streetAddress.shortText;
     }
-  }
-
-  // Checking correct place NAME
-  if (
-    address.country !== result.displayName
-    && countryLongName !== result.displayName
-    && address.region !== result.displayName
-    && regionLongName !== result.displayName
-    && address.city !== result.displayName
-    && streetShortName !== result.displayName
-    && address.zip !== result.displayName
-    && address.street !== result.displayName
-  ) {
-    if (config.name && config.name.visible !== false) {
-      address.name = result.displayName;
-    }
-
-  } else {
-    address.name = '';
   }
 
   return address;
