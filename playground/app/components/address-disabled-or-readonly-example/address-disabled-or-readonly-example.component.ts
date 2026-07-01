6dc7fdf54180fc262043761f9dc7c0ee510c4bd1
Raymond Gigliotti <ray@firestitch.com>
2026-07-01 10:16:09 -0400

    Refactor address-disabled-or-readonly-example component imports for improved organization and maintainability.

--- playground/app/components/address-disabled-or-readonly-example/address-disabled-or-readonly-example.component.ts
+++ playground/app/components/address-disabled-or-readonly-example/address-disabled-or-readonly-example.component.ts
@@ -1,24 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import {
   AddressFormat, FsAddress,
   FsAddressConfig,
 } from '@firestitch/address';
-import { FormsModule } from '@angular/forms';
-import { FsAddressPickerComponent } from '../../../../src/app/components/address-picker/address-picker.component';
+
 import { FsAddressFormatComponent } from '../../../../src/app/components/address-format/address-format.component';
+import { FsAddressPickerComponent } from '../../../../src/app/components/address-picker/address-picker.component';
 
 
 @Component({
-    selector: 'address-disabled-or-readonly-example',
-    templateUrl: './address-disabled-or-readonly-example.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [
-        FormsModule,
-        FsAddressPickerComponent,
-        FsAddressFormatComponent,
-    ],
+  selector: 'address-disabled-or-readonly-example',
+  templateUrl: './address-disabled-or-readonly-example.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    FormsModule,
+    FsAddressPickerComponent,
+    FsAddressFormatComponent,
+  ],
 })
 export class AddressDisabledOrReadonlyExampleComponent {
 
